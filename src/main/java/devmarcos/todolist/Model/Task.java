30023131ed770c204cbb4ad9a4fb0a72a1e112e8
MarcosMilezarek <marcosmilezarek@gmail.com>
2026-06-24 15:46:07 -0300

    chore: initial project setup

---
+++ src/main/java/devmarcos/todolist/Model/Task.java
@@ -0,0 +1,59 @@
+package devmarcos.todolist.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+    private String status;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private LocalDateTime data_limite;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public LocalDateTime getData_limite() {
+        return data_limite;
+    }
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public void setData_limite(LocalDateTime data_limite) {
+        this.data_limite = data_limite;
+    }
+
+    public Task(Integer id, String descricao, String status, LocalDateTime data_limite) {
+        this.id = id;
+        this.descricao = descricao;
+        this.status = status;
+        this.data_limite = data_limite;
+    }
+}
+)
---
+++ src/main/java/devmarcos/todolist/Model/Usuario.java
@@ -0,0 +1,46 @@
+package devmarcos.todolist.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+@Entity
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Usuario(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+}
